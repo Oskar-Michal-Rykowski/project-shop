d7c6a286630c778bc928e3f761190bd2aea28831
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2022-01-24 19:55:20 +0100

    Add baner to product view

--- src/components/features/Products/Products.js
+++ src/components/features/Products/Products.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
-import Icon from '@mui/material/Icon';
-import { loadCSS } from 'fg-loadcss';
-import { Link } from 'react-router-dom';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
+
 // import Tooltip from '@mui/material/Tooltip';
 // import Avatar from '@mui/material/Avatar';
 import styles from './Products.module.scss';
--- src/components/views/Product/Product.js
+++ src/components/views/Product/Product.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-import { Link } from 'react-router-dom';
-import styles from './Product.module.scss';
 import { connect } from 'react-redux';
-import { getProductById } from '../../../redux/productsRedux';
+import { getProducts } from '../../../redux/productsRedux';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styles from './Product.module.scss';
 
-const Component = ({ product }) => {
+const Component = ({ products }) => {
+  console.log('product', products);
+
+  const url = window.location.href;
+  const id = url.substring(url.lastIndexOf('/') + 1);
+  const product = products.find((product) => product.id === id);
   console.log('product', product);
-  return <h1>PRODUCT</h1>;
+
+  return (
+    <Container maxWidth="xl">
+      <div className={styles.baner}>
+        <div className={styles.splash}>
+          <Typography className={styles.slogan} varian={'h6'}>
+            Free delivery from <span className={styles.price}>PLN 100</span>!
+          </Typography>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 Component.propTypes = {
-  product: PropTypes.object,
+  products: PropTypes.object,
 };
 
+// Dlaczego ownProps.match.params.id nie działa?
+
 const mapStateToProps = (state, ownProps) => {
-  console.log('ownProps', ownProps);
   return {
-    product: getProductById(state, ownProps.match.params.id),
+    products: getProducts(state),
   };
 };
 
-const mapDispatchToProps = (state, ownProps) => {
-  console.log('ownProps 2', ownProps);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  // console.log('ownProps 2', ownProps.match.params.id);
   return {};
 };
 
